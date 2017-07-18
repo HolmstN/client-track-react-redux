d951b36cf5813162235024f9dca5d75c04f9a8f7
Nick Holmstedt <nrholmstedt@gmail.com>
2017-07-18 19:52:21 +0000

    Reworked successfully

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,15 @@ import { combineReducers } from 'redux'
 import clients from './clients'
 import { visibilityFilter } from './visibilityFilter'
 import { searchClientsByText } from './searchClientsByText'
+import { changeDashboardView } from './changeDashboardView'
+
 import { reducer as formReducer } from 'redux-form'
 import showClientForm from './showClientForm'
 
  
 const rootReducer = combineReducers({
     clients,
+    dashboardView: changeDashboardView,
     searchText: searchClientsByText,
     visibilityFilter,
     clientFormOpen: showClientForm,
